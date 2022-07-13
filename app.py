35aed7106fe101e8962900cd831513ec3df0470f
Rebecca Brietzke <rnbrietzke@outlook.com>
2022-07-13 18:39:35 -0500

    create register form and register and secret routes

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import os
 from flask import Flask, render_template, redirect, session, flash
 from flask_debugtoolbar import DebugToolbarExtension
-from models import connect_db, db
+from models import connect_db, db, User
+from forms import RegistrationForm
+from sqlalchemy.exc import IntegrityError
 
 
 app = Flask(__name__)
@@ -19,4 +21,32 @@ connect_db(app)
 
 @app.route('/')
 def home_page():
-    return render_template('index.html')
+    return redirect('/register')
+
+
+@app.route('/register', methods=["GET", "POST"])
+def register_user():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        first_name = form.first_name.data
+        last_name = form.last_name.data
+        email = form.email.data
+        username = form.username.data
+        password = form.password.data
+        new_user = User.register(username, password, email, first_name, last_name)
+        db.session.add(new_user)
+        try:
+            db.session.commit()
+        except IntegrityError:
+            form.username.errors.append("Username is taken. Please pick a different username")
+            return render_template('register.html', form=form)
+        session["user_id"] = new_user.username
+        flash(f"Welcome {new_user.first_name}, you successfully created your account")
+        return redirect('/secret')
+
+    return render_template('register.html', form=form)
+
+
+@app.route('/secret')
+def secret_route():
+    return "<h1>You made it!</h1>"
---
+++ forms.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField
+from wtforms.validators import InputRequired, Email
+
+class RegistrationForm(FlaskForm):
+    first_name = StringField("First Name", validators=[InputRequired()])
+    last_name = StringField("Last Name", validators=[InputRequired()])
+    email = StringField("Email", validators=[InputRequired(), Email("This field requires a valid email address")])
+    username = StringField("Username", validators=[InputRequired()])
+    password = PasswordField("Password", validators=[InputRequired()])
