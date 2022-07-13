99d1194c52d1dc87d2bbca78cd602244318fe42b
Rebecca Brietzke <rnbrietzke@outlook.com>
2022-07-13 17:31:14 -0500

    add user model and class method for registration

--- models.py
+++ models.py
@@ -11,3 +11,35 @@ def connect_db(app):
 
     db.app = app
     db.init_app(app)
+
+class User(db.Model):
+    """User table database model"""
+
+    __tablename__ = "users"
+
+    username = db.Column(db.String(20), primary_key=True, nullable=False)
+    password = db.Column(db.Text, nullable=False)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    first_name = db.Column(db.String(30), nullable=False)
+    last_name = db.Column(db.String(30), nullable=False)
+
+    @classmethod
+    def register(cls, username, pwd, email, first_name, last_name):
+        """Register user with hashed password and return user"""
+
+        hashed_pwd = bcrypt.generate_password_hash(pwd)
+        hashed_utf8 = hashed_pwd.decode(("utf8"))
+
+        return cls(username=username, password=hashed_utf8, email=email, first_name=first_name,last_name=last_name)
+
+        @classmethod
+        def authenticate(cls, username, pwd):
+            """
+            Validate that user exists and password is correct
+            Return user if valid; else return False
+            """
+
+
+
+
+
